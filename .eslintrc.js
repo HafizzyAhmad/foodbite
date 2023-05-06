a0f53e0254e0354febdc69d08de58901db162e4c
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-05-06 15:48:13 +0800

    rule for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,35 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'plugin:prettier/recommended'],
+  extends: [
+    '@react-native-community',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
+        // rn
+        'react-native/no-inline-styles': 'error',
+        'react-native/no-unused-styles': 'error',
+
+        // general
         'no-shadow': 'off',
         'no-undef': 'off',
+        'no-console': 'error',
         'prettier/prettier': 'error',
+
+        // typescript
+        '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-namespace': [
+          'error',
+          { allowDeclarations: true },
+        ],
       },
     },
   ],
