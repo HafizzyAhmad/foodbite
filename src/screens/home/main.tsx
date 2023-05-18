6997e769c6d1c6f1691bcd84ea2d45dcb7a051df
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-05-19 02:26:29 +0800

    temp logout

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import common from '../../styles/common';
+import { useStore } from '../../hooks';
+import { logout } from '../../stores/app';
 
 const HomeMain: React.FC = () => {
+  const [, dispatch] = useStore();
   return (
     <View style={common.flexCenter}>
-      <Text>This is Home</Text>
+      <Text onPress={() => dispatch(logout())}>This is Home</Text>
     </View>
   );
 };
