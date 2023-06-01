8741ed26fe70d306796c7be2045447de664854eb
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-06-02 02:15:50 +0800

    init mapbox

---
+++ env.js
@@ -0,0 +1,7 @@
+// MAPBOX
+const MAPBOX_TOKEN =
+  'sk.eyJ1IjoibWhhZml6enkiLCJhIjoiY2xpNHZxdnFzMnZ3MTNlbnQ3aWg3ZndldyJ9.Wn3Th-0N1fntQwC3mXjEbQ';
+
+export default {
+  MAPBOX_TOKEN,
+};
--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import common from '../../styles/common';
 import { useStore } from '../../hooks';
 import { logout } from '../../stores/app';
+import ENV from '../../../env';
+import Mapbox from '@rnmapbox/maps';
+
+Mapbox.setAccessToken(ENV.MAPBOX_TOKEN);
 
 const HomeMain: React.FC = () => {
+  useEffect(() => {
+    Mapbox.setTelemetryEnabled(false);
+  });
   const [, dispatch] = useStore();
   return (
     <View style={common.flexCenter}>
+      <Mapbox.MapView style={{ height: 300, width: 300 }} />
       <Text onPress={() => dispatch(logout())}>This is Home</Text>
     </View>
   );
