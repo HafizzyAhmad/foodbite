de328e336ca762ec7bf6d5e6421274852eb2d58f
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-06-06 03:03:54 +0800

    fix map view home screen

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,22 +1,108 @@
 // Integration of Google map in React Native using react-native-maps
 // https://aboutreact.com/react-native-map-example/
 // Import React
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 // Import required components
 import {
   Animated,
   Dimensions,
-  SafeAreaView,
   StyleSheet,
   Text,
   View,
+  PermissionsAndroid,
+  Platform,
+  Pressable,
 } from 'react-native';
 // Import Map and Marker
 import MapView, { Circle, Marker } from 'react-native-maps';
 import { text } from '../../styles';
+import Geolocation from '@react-native-community/geolocation';
+import Feather from 'react-native-vector-icons/Feather';
+import * as geolib from 'geolib';
 
 const HomeMain = () => {
-  const { width } = Dimensions.get('window');
+  const mapRef = useRef(null);
+  const { width, height } = Dimensions.get('window');
+
+  const [currentRegion, setCurrentRegion] = useState<any>(null);
+  const [distance, setDistance] = useState<number>(0);
+
+  const INITIAL_REGION = {
+    latitude: 3.1161,
+    longitude: 101.665742,
+    latitudeDelta: 0.006,
+    longitudeDelta: 0.003,
+  };
+
+  const LATITUDE_DELTA = 0.006;
+  const LONGITUDE_DELTA = 0.003;
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Enable access location.',
+          message: 'foodbite needs access to your location',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+    }
+
+    const getCurrentLocation = () => {
+      Geolocation.getCurrentPosition(
+        async info => {
+          const loadSelectedMarker = {
+            latitude: info.coords.latitude,
+            longitude: info.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          };
+
+          setCurrentRegion(loadSelectedMarker);
+          console.log('READY TO CALL API: ', loadSelectedMarker);
+
+          calculateDistance(
+            loadSelectedMarker.latitudeDelta,
+            loadSelectedMarker.longitudeDelta,
+          );
+        },
+        errorLoc => {
+          console.log('geolocation info error', errorLoc);
+          setCurrentRegion(INITIAL_REGION);
+
+          console.log('CALL API USING INITIAL REGION: ', INITIAL_REGION);
+
+          calculateDistance(
+            INITIAL_REGION.latitudeDelta,
+            INITIAL_REGION.longitudeDelta,
+          );
+        },
+        { enableHighAccuracy: false, timeout: 3000, maximumAge: 10000 },
+      );
+    };
+
+    getCurrentLocation();
+  }, []);
+
+  const calculateDistance = (latitudeDelta, longitudeDelta) => {
+    const screenDiagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+    const distancePerPixel = geolib.getDistance(
+      { latitude: 0, longitude: 0 },
+      { latitude: latitudeDelta, longitude: longitudeDelta },
+    );
+
+    const distance = (screenDiagonal * (distancePerPixel / width)) / 1000;
+    setDistance(distance);
+  };
+
+  const handleRegionChangeComplete = region => {
+    setCurrentRegion(region);
+    calculateDistance(region.latitudeDelta, region.longitudeDelta);
+  };
+
   const locations = [
     {
       name: 'Location A',
@@ -29,13 +115,6 @@ const HomeMain = () => {
       longitude: 101.71523119718769,
     },
   ];
-
-  // const [region, setRegion] = useState({
-  //   latitude: locations[0].latitude,
-  //   longitude: locations[0].longitude,
-  //   latitudeDelta: 0.0922,
-  //   longitudeDelta: 0.0421,
-  // });
 
   const [carouselOffset] = useState(new Animated.Value(0));
 
@@ -60,40 +139,48 @@ const HomeMain = () => {
     carouselOffset.setValue(0);
   }, [carouselOffset]);
 
-  const [currentLocation, setCurrentLocation] = useState<any>({
-    latitude: 3.172157809479211,
-    longitude: 101.72122372314334,
-    latitudeDelta: 0.05,
-    longitudeDelta: 0.05,
-  }); //Please enhance to use user's location on init
-  const handleRegionChange = (region: { latitude: any; longitude: any }) => {
-    const { latitude, longitude } = region;
-    setCurrentLocation({ latitude, longitude });
-  };
+  console.log('CURRENT LOCATION: ', currentRegion);
+  console.log('MAP DISTANCE: ', distance);
 
-  console.log('CURRENT LOCATION: ', currentLocation);
+  const handleSelectLocation = location => {
+    console.log('GO TO SCREEN DONATION/REQUEST INFO', location);
+  };
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} onRegionChange={handleRegionChange}>
-        {currentLocation && (
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        // onRegionChange={handleRegionChange}
+        initialRegion={INITIAL_REGION}
+        region={currentRegion}
+        showsUserLocation
+        onRegionChangeComplete={region => {
+          if (currentRegion !== region) {
+            setCurrentRegion(region);
+          }
+          handleRegionChangeComplete(region);
+        }}>
+        {currentRegion && (
           <Circle
-            center={currentLocation}
+            center={currentRegion}
             radius={1000}
             // fillColor="red"
-            // strokeColor="black"
+            strokeColor="transparent"
             // strokeWidth={2}
           />
         )}
         {locations.map((location, index) => (
+          //add logic to check type is donation/request
           <Marker
             key={index}
             coordinate={{
               latitude: location.latitude,
               longitude: location.longitude,
             }}
-            onPress={() => handleMarkerPress(index)}
-          />
+            onPress={() => handleMarkerPress(index)}>
+            <Feather name="user" size={20} />
+          </Marker>
         ))}
       </MapView>
       <Animated.View
@@ -103,7 +190,17 @@ const HomeMain = () => {
         ]}>
         {locations.map((location, index) => (
           <View key={index} style={carouselItemStyle.carouselItem}>
-            <Text style={text.whiteBodyHighlight}>{location.name}</Text>
+            <Pressable
+              onPress={() => handleSelectLocation(location)}
+              style={{
+                backgroundColor: 'white',
+                borderRadius: 10,
+                width: '90%',
+                height: 100,
+                padding: 15,
+              }}>
+              <Text style={[text.blackBodyHighlight]}>{location.name}</Text>
+            </Pressable>
           </View>
         ))}
       </Animated.View>
@@ -122,7 +219,7 @@ const styles = StyleSheet.create({
   },
   carouselContainer: {
     // width: '90%',
-    backgroundColor: '#20DD20',
+    // backgroundColor: '#20DD20',
     flexDirection: 'row',
     position: 'absolute',
     bottom: 0,
