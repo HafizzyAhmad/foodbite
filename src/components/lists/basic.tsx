6ac036215bf7853085f8af4309fb6c07566b8a4f
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-06-13 02:31:52 +0800

    add basic list component

---
+++ src/components/lists/basic.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Basic = () => {
+  return (
+    <>
+      <Text>Title</Text>
+      <Text>Description</Text>
+    </>
+  );
+};
+
+export default Basic;
