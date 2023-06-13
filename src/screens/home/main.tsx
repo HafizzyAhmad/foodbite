1a209fcedf8eef40d2e8ede7375c267bb10076b7
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-06-14 00:57:16 +0800

    integrate api to home screen

--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -12,6 +12,7 @@ import {
   PermissionsAndroid,
   Platform,
   Pressable,
+  Alert,
 } from 'react-native';
 // Import Map and Marker
 import MapView, { Circle, Marker } from 'react-native-maps';
@@ -19,13 +20,25 @@ import { text } from '../../styles';
 import Geolocation from '@react-native-community/geolocation';
 import Feather from 'react-native-vector-icons/Feather';
 import * as geolib from 'geolib';
+import { useStore } from '../../hooks';
+import {
+  getPostByCoordinate,
+  getPostByCoordinateFailed,
+  getPostByCoordinateSuccess,
+} from '../../stores/post';
+import PostAPI from '../../api/post';
 
 const HomeMain = () => {
   const mapRef = useRef(null);
   const { width, height } = Dimensions.get('window');
+  const [globalState, dispatch] = useStore();
+
+  const { app } = globalState;
+  const postAPI = new PostAPI(app.token);
 
   const [currentRegion, setCurrentRegion] = useState<any>(null);
-  const [distance, setDistance] = useState<number>(0);
+  const [distance, setDistance] = useState<number>(5);
+  const [dataOnMap, setDataOnMap] = useState<any[]>([]);
 
   const INITIAL_REGION = {
     latitude: 3.1161,
@@ -51,7 +64,7 @@ const HomeMain = () => {
       );
     }
 
-    const getCurrentLocation = () => {
+    const getCurrentLocation = async () => {
       Geolocation.getCurrentPosition(
         async info => {
           const loadSelectedMarker = {
@@ -62,7 +75,7 @@ const HomeMain = () => {
           };
 
           setCurrentRegion(loadSelectedMarker);
-          console.log('READY TO CALL API: ', loadSelectedMarker);
+          // console.log('READY TO CALL API: ', loadSelectedMarker);
 
           calculateDistance(
             loadSelectedMarker.latitudeDelta,
@@ -73,7 +86,7 @@ const HomeMain = () => {
           console.log('geolocation info error', errorLoc);
           setCurrentRegion(INITIAL_REGION);
 
-          console.log('CALL API USING INITIAL REGION: ', INITIAL_REGION);
+          // console.log('CALL API USING INITIAL REGION: ', INITIAL_REGION);
 
           calculateDistance(
             INITIAL_REGION.latitudeDelta,
@@ -98,10 +111,29 @@ const HomeMain = () => {
     setDistance(distance);
   };
 
-  const handleRegionChangeComplete = region => {
+  const handleRegionChangeComplete = (region: any) => {
     setCurrentRegion(region);
     calculateDistance(region.latitudeDelta, region.longitudeDelta);
+    dragMapEnds();
   };
+
+  async function dragMapEnds() {
+    dispatch(getPostByCoordinate());
+    try {
+      const param = {
+        centerLat: currentRegion.latitude,
+        centerLong: currentRegion.longitude,
+        distance: distance,
+      };
+      const res = await postAPI.getPostByCoordinate(param);
+      setDataOnMap(res);
+      dispatch(getPostByCoordinateSuccess());
+      console.log('CHHECK RES: ', res[0]);
+    } catch (error) {
+      Alert.alert('Oh uh!, Error to load location. Please try again');
+      dispatch(getPostByCoordinateFailed());
+    }
+  }
 
   const locations = [
     {
@@ -170,40 +202,52 @@ const HomeMain = () => {
             // strokeWidth={2}
           />
         )}
-        {locations.map((location, index) => (
-          //add logic to check type is donation/request
-          <Marker
-            key={index}
-            coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-            }}
-            onPress={() => handleMarkerPress(index)}>
-            <Feather name="user" size={20} />
-          </Marker>
-        ))}
+        {dataOnMap.length > 0 &&
+          dataOnMap.map((location, index) => {
+            const { geoLocation } = location;
+            return (
+              //add logic to check type is donation/request
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: parseFloat(geoLocation.latitude),
+                  longitude: parseFloat(geoLocation.longitude),
+                }}
+                onPress={() => handleMarkerPress(index)}>
+                <Feather name="user" size={20} />
+              </Marker>
+            );
+          })}
       </MapView>
-      <Animated.View
-        style={[
-          styles.carouselContainer,
-          { transform: [{ translateX: carouselOffset }] },
-        ]}>
-        {locations.map((location, index) => (
-          <View key={index} style={carouselItemStyle.carouselItem}>
-            <Pressable
-              onPress={() => handleSelectLocation(location)}
-              style={{
-                backgroundColor: 'white',
-                borderRadius: 10,
-                width: '90%',
-                height: 100,
-                padding: 15,
-              }}>
-              <Text style={[text.blackBodyHighlight]}>{location.name}</Text>
-            </Pressable>
-          </View>
-        ))}
-      </Animated.View>
+      {dataOnMap.length > 0 && (
+        <Animated.View
+          style={[
+            styles.carouselContainer,
+            { transform: [{ translateX: carouselOffset }] },
+          ]}>
+          {dataOnMap.map((location, index) => {
+            const { donation } = location;
+            return (
+              <View key={index} style={carouselItemStyle.carouselItem}>
+                <Pressable
+                  onPress={() => handleSelectLocation(location)}
+                  style={{
+                    backgroundColor: 'white',
+                    borderRadius: 10,
+                    width: '90%',
+                    height: 100,
+                    padding: 15,
+                  }}>
+                  <Text style={[text.blackBodyHighlight]}>{donation.name}</Text>
+                  <Text style={[text.blackBodyReg]}>
+                    {donation.description}
+                  </Text>
+                </Pressable>
+              </View>
+            );
+          })}
+        </Animated.View>
+      )}
     </View>
   );
 };
