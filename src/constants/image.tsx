f42c835448e0fc0e9cfd242b72ff9f3e1f3c24c5
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-07-01 00:26:39 +0800

    add icon map marker

--- src/constants/image.tsx
+++ src/constants/image.tsx
@@ -7,6 +7,10 @@ export default {
   //icon
   donationIcon: require('../assets/icons/icon-donate.png'),
   requestHelpIcon: require('../assets/icons/icon-help.png'),
+  donationMapIcon: require('../assets/icons/icon-donate-map.png'),
+  donationMapSelectedIcon: require('../assets/icons/icon-donate-map-selected.png'),
+  requestMapIcon: require('../assets/icons/icon-help-map.png'),
+  requestMapSelectedIcon: require('../assets/icons/icon-help-map-selected.png'),
 
   //animation
   successAnimation: require('../assets/animations/completed.json'),
