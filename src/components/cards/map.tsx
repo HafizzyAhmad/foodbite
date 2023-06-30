674bbc160de5091c10d7aa0ca1b9af9518a23937
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-07-01 02:31:42 +0800

    enhance map card info to include date

--- src/components/cards/map.tsx
+++ src/components/cards/map.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ActivityIndicator, Pressable, Text } from 'react-native';
 import { card, text } from '../../styles';
 import { View } from 'react-native';
+import TextButton from '../buttons/textbutton';
+import Formatter from '../../utils/formatter';
 
 type IMapCard = {
   data: any;
@@ -18,8 +20,18 @@ const MapCard = ({ data, nav, fetching }: IMapCard) => {
         </View>
       ) : (
         <Pressable onPress={nav} style={card.postInfoCard}>
-          <Text style={[text.blackBodyHighlight]}>{data.name}</Text>
-          <Text style={[text.blackBodyReg]}>{data.description}</Text>
+          <Text numberOfLines={1} style={[text.blackBodyHighlight]}>
+            {data.donation.name}
+          </Text>
+          <Text numberOfLines={1} style={[text.blackBodyReg]}>
+            {data.address}
+          </Text>
+          <Text style={[text.greyLabelText]}>
+            {`Available from ${Formatter.dateTime(
+              data.statusAvailability.startDateTime,
+            )}\nto ${Formatter.dateTime(data.statusAvailability.endDateTime)}`}
+          </Text>
+          <TextButton caption={`View ${data.type}`} />
         </Pressable>
       )}
     </View>
