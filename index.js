b2029299510d4e7b4a6166cd06bd14df44fe6d7c
Hafizzy <hafizzy.ahmad@tm.com.my>
2023-07-05 04:34:24 +0800

    init image zoom, reanimated and gesture handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+import 'react-native-gesture-handler'; // installation guide for reanimated carousell
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Controller from './routes/controller';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StoreProvider } from './hooks/use-store';
 import { Platform, StatusBar } from 'react-native';
+import { common } from './styles';
 
 const App: React.FC = () => {
   return (
     <StoreProvider>
-      <Controller />
-      <StatusBar
-        barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
-        animated={true}
-        backgroundColor="black"
-      />
+      <GestureHandlerRootView style={common.basicFlex}>
+        <Controller />
+        <StatusBar
+          barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
+          animated={true}
+          backgroundColor="black"
+        />
+      </GestureHandlerRootView>
     </StoreProvider>
   );
 };
